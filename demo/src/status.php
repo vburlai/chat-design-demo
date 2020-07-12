ef33d223937d5411bce50fa56465275608729f11
Vitalii Burlai <vitaly.burlai@gmail.com>
2020-07-12 15:34:23 +0100

    MQ status reported using MQ management api

--- demo/src/status.php
+++ demo/src/status.php
@@ -1,10 +1,15 @@
 <?php
 $m = new Memcached();
-$server = $_ENV['MEMCACHED_ADDR'];
-if (!is_string($server)) {
+$cache_server = $_ENV['MEMCACHED_ADDR'];
+if (!is_string($cache_server)) {
     die("MEMCACHED_ADDR env variable is required");
 }
-$arr = explode(":", $server);
+$mq_api = $_ENV['MESSAGE_QUEUE_HTTP_API_ADDR'];
+if (!is_string($mq_api)) {
+    die("MESSAGE_QUEUE_HTTP_API_ADDR env variable is required");
+}
+
+$arr = explode(":", $cache_server);
 $m->addServer($arr[0], $arr[1]);
 
 $rooms = $m->getMulti(array('room_room-1', 'room_room-2'));
@@ -41,11 +46,29 @@ if($room2) {
 $client_connected = "<span style='color: green; font-weight: bold;'>(connected)</span>";
 $client_disconnected = "<span style='color: grey;'>(disconnected)</span>";
 
+$message_queue = "";
+
+$c = curl_init();
+curl_setopt($c, CURLOPT_URL, $mq_api.'/queues');
+curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($c, CURLOPT_HTTPHEADER, array('Authorization: Basic '.base64_encode('guest:guest')));
+$mq_queues = curl_exec($c);
+curl_close($c);
+
+if ($mq_queues) {
+    $arr = json_decode($mq_queues);
+    $message_queue = "Active queues:";
+    foreach ($arr as $item) {
+        $message_queue = $message_queue."<br>".$item->name;
+    }
+
+}
+
 $status = array(
     "app-server-1" => $app_server_1 ? "Connected:<br>&nbsp;".implode("<br>&nbsp;",$app_server_1) : "",
     "app-server-2" => $app_server_2 ? "Connected:<br>&nbsp;".implode("<br>&nbsp;",$app_server_2) : "",
     "database" => "",
-    "message-queue" => "",
+    "message-queue" => $message_queue,
     "client-1" => $clients['client-1'] ? $client_connected : $client_disconnected,
     "client-2" => $clients['client-2'] ? $client_connected : $client_disconnected,
     "client-3" => $clients['client-3'] ? $client_connected : $client_disconnected,
