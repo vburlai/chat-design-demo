65c2362ca85a054f8bb7dfffdcff504baefc5d52
Vitalii Burlai <vitaly.burlai@gmail.com>
2020-07-16 20:26:10 +0100

    Display chat_users table

--- demo/src/status.php
+++ demo/src/status.php
@@ -16,6 +16,22 @@ $mq_admin_pass = $_ENV['MQ_ADMIN_PASSWORD'];
 if (!is_string($mq_admin_pass)) {
     die("MQ_ADMIN_PASSWORD env variable is required");
 }
+$mysql_host = $_ENV['MYSQL_HOST'];
+if (!is_string($mysql_host)) {
+    die("MYSQL_HOST env variable is required");
+}
+$mysql_db = $_ENV['MYSQL_DATABASE'];
+if (!is_string($mysql_db)) {
+    die("MYSQL_DATABASE env variable is required");
+}
+$mysql_user = $_ENV['MYSQL_USER'];
+if (!is_string($mysql_user)) {
+    die("MYSQL_USER env variable is required");
+}
+$mysql_pass = $_ENV['MYSQL_PASSWORD'];
+if (!is_string($mysql_pass)) {
+    die("MYSQL_PASSWORD env variable is required");
+}
 
 $arr = explode(":", $cache_server);
 $m->addServer($arr[0], $arr[1]);
@@ -72,10 +88,29 @@ if ($mq_queues) {
 
 }
 
+$mysql = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
+$users = $mysql->query("SELECT * FROM chat_users");
+$database="";
+if ($users) {
+    $database = $database."<div style='width: 100%; overflow: auto; border: 1px solid grey'>";
+    $database = $database."<table style='font-size: 10px;'><tr><th>clientId</th><th>Username</th><th>Server</th><th>Room</th></tr>";
+    while($obj = $users->fetch_object()) {
+        $database = $database.'<tr>';
+        $database = $database.'<td>'.$obj->clientId.'</td>';
+        $database = $database.'<td>'.$obj->username.'</td>';
+        $database = $database.'<td>'.$obj->hostname.'</td>';
+        $database = $database.'<td>'.$obj->room.'</td>';
+        // $database = $database.'<td>'.$obj->joined;
+        $database = $database.'</tr>';
+    }
+    $database = $database."</table>";
+    $database = $database."</div>";
+}
+
 $status = array(
-    "app-server-1" => $app_server_1 ? "Connected:<br>&nbsp;".implode("<br>&nbsp;",$app_server_1) : "",
-    "app-server-2" => $app_server_2 ? "Connected:<br>&nbsp;".implode("<br>&nbsp;",$app_server_2) : "",
-    "database" => "",
+    "app-server-1" => $app_server_1 ? "<div style='height: 100%; overflow: auto;'>Connected:<br>&nbsp;".implode("<br>&nbsp;",$app_server_1)."</div>" : "",
+    "app-server-2" => $app_server_2 ? "<div style='height: 100%; overflow: auto;'>Connected:<br>&nbsp;".implode("<br>&nbsp;",$app_server_2)."</div>" : "",
+    "database" => $database,
     "message-queue" => $message_queue,
     "client-1" => $clients['client-1'] ? $client_connected : $client_disconnected,
     "client-2" => $clients['client-2'] ? $client_connected : $client_disconnected,
