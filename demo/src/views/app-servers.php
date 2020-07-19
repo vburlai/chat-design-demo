0823da808befcb4a6c8072523098bcf18176cd5e
Vitalii Burlai <vitaly.burlai@gmail.com>
2020-07-19 21:31:29 +0100

    Styling

--- demo/src/views/app-servers.php
+++ demo/src/views/app-servers.php
@@ -12,9 +12,18 @@ function getAppServersHTML($hostnames, $rooms) {
     $clients_str = implode('</td><td>', $clients);
     
     return <<<EOD
-<style> table th { width: 50%; } </style>
-<div style='height: 100%; overflow: auto;'>
+<style>
+table th { width: 50%; }
+.wrapper {
+    width: calc(100% - 3px);
+    overflow: auto;
+    border: 1px solid grey;
+    margin-left: 1px;
+    box-sizing: border-box;
+}
+</style>
 Clients:<br>
+<div class="wrapper">
 <table>
 <tr><th>$hostnames_str</th></tr>
 <tr><td>$clients_str</td></tr>
